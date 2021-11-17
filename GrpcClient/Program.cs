488d77f87c5abb7bcc8bd600595ec1bc71564905
htopo <htrnbs@gmail.com>
2021-11-16 23:53:09 -0300

    grpc sever y client funcionando

--- GrpcClient/Program.cs
+++ GrpcClient/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
 
 namespace GrpcClient
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // The port number(5001) must match the port of the gRPC server.
+            using var channel = GrpcChannel.ForAddress("https://localhost:7428");
+            var client = new Greeter.GreeterClient(channel);
+            string input = "";
+            while (!input.Equals("exit", StringComparison.InvariantCultureIgnoreCase))
+            {
+                input = Console.ReadLine();
+                HelloReply reply = await client.SayHelloAsync(new HelloRequest 
+                { 
+                    Name = input 
+                });
+                Console.WriteLine(reply);
+            }
+            
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
         }
     }
 }
